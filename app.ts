002cf38d75ac6fe4d5711c37efdf617882051880
Kamil <kamgre7@gmail.com>
2023-05-24 22:51:43 +0200

    Added routing

--- app.ts
+++ app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import { config } from './config/default';
+import { translate } from './routes/translate';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/translate', translate);
 
 app.listen(config.port, config.hostName, () => {
   console.log(`Listening on port ${config.host}:${config.port}`);
