620b30042ccaa7606bc31c144d9c5d84207f8328
Kamil <kamgre7@gmail.com>
2023-06-04 22:30:29 +0200

    added type

--- config/default.ts
+++ config/default.ts
@@ -1,22 +1,43 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-export const config = {
-  port: Number(process.env.PORT) || 3001,
-  host: process.env.HOST,
-  hostName: process.env.HOST_NAME,
-  cacheFilePath: process.env.CACHE_FILE_PATH,
+interface IConfig {
+  port: number;
+  host: string;
+  hostName: string;
+  cacheFilePath: string;
   googleCloud: {
-    type: process.env.GOOGLE_CLOUD_TYPE,
-    project_id: process.env.GOOGLE_PROJECT_ID,
-    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    client_id: process.env.GOOGLE_CLIENT_ID,
-    auth_uri: process.env.GOOGLE_AUTH_URI,
-    token_uri: process.env.GOOGLE_TOKEN_URL,
-    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
-    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+    type: string;
+    project_id: string;
+    private_key_id: string;
+    private_key: string;
+    client_email: string;
+    client_id: string;
+    auth_uri: string;
+    token_uri: string;
+    auth_provider_x509_cert_url: string;
+    client_x509_cert_url: string;
+    universe_domain: string;
+  };
+}
+
+export const config: IConfig = {
+  port: Number(process.env.PORT) || 3001,
+  host: process.env.HOST!,
+  hostName: process.env.HOST_NAME!,
+  cacheFilePath: process.env.CACHE_FILE_PATH!,
+  googleCloud: {
+    type: process.env.GOOGLE_CLOUD_TYPE!,
+    project_id: process.env.GOOGLE_PROJECT_ID!,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID!,
+    private_key: process.env.GOOGLE_PRIVATE_KEY!,
+    client_email: process.env.GOOGLE_CLIENT_EMAIL!,
+    client_id: process.env.GOOGLE_CLIENT_ID!,
+    auth_uri: process.env.GOOGLE_AUTH_URI!,
+    token_uri: process.env.GOOGLE_TOKEN_URL!,
+    auth_provider_x509_cert_url:
+      process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL!,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL!,
+    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN!,
   },
 };
