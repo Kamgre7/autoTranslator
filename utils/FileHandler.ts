4a5c53370f70c1530d73ead7b9db30ee7bbe1b39
Kamil <kamgre7@gmail.com>
2023-06-05 23:16:39 +0200

    Added create directory method

--- utils/FileHandler.ts
+++ utils/FileHandler.ts
@@ -1,28 +1,55 @@
-import { readFile, writeFile } from 'node:fs/promises';
+import { mkdir, readFile, writeFile } from 'node:fs/promises';
 import { config } from '../config/default';
 import { cachedFile } from './CacheTranslator';
 
 export interface IFileHandler {
-  readFile(): Promise<cachedFile>;
-  writeFile(data: cachedFile): Promise<void>;
+  readFile(fromToTargetShortcut: string): Promise<cachedFile>;
+  writeFile(data: cachedFile, fromToTargetShortcut: string): Promise<void>;
+  createCacheDirectoryIfNotExist(): Promise<void>;
 }
 
 export class FileHandler implements IFileHandler {
-  async readFile(): Promise<cachedFile> {
-    const file = await readFile(config.cacheFilePath, {
-      encoding: 'utf-8',
-    });
+  async readFile(fromToTargetShortcut: string): Promise<cachedFile> {
+    try {
+      const file = await readFile(
+        `${config.cacheFilePath}/${fromToTargetShortcut}.json`,
+        {
+          encoding: 'utf-8',
+        }
+      );
 
-    const parsedFile: cachedFile = JSON.parse(file);
+      const parsedFile: cachedFile = JSON.parse(file);
 
-    return parsedFile;
+      return parsedFile;
+    } catch (error) {
+      if ((error as any).code === 'ENOENT') {
+        await this.writeFile({}, fromToTargetShortcut);
+
+        return {};
+      }
+      throw new Error((error as Error).message);
+    }
   }
 
-  async writeFile(data: cachedFile): Promise<void> {
+  async writeFile(
+    data: cachedFile,
+    fromToTargetShortcut: string
+  ): Promise<void> {
     try {
-      await writeFile(config.cacheFilePath, JSON.stringify(data));
+      await writeFile(
+        `${config.cacheFilePath}/${fromToTargetShortcut}.json`,
+        JSON.stringify(data)
+      );
     } catch (error) {
       throw new Error((error as Error).message);
+    }
+  }
+
+  async createCacheDirectoryIfNotExist(): Promise<void> {
+    try {
+      await mkdir(config.cacheFilePath, { recursive: true });
+    } catch (error) {
+      console.error((error as Error).message);
     }
   }
 }
