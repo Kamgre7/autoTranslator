41f2eaaa028ddad79df251982126cd248d0ea460
Kamil <kamgre7@gmail.com>
2023-06-11 22:06:34 +0200

    Added env.example

--- app.ts
+++ app.ts
@@ -1,15 +1,12 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
 import { config } from './config/default';
 import { translateRouter } from './routes/translateRouter';
-
-dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/translate', translateRouter);
+app.use('/', translateRouter);
 
 app.listen(config.port, config.hostName, () => {
   console.log(`Listening on port ${config.host}:${config.port}`);
