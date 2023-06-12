50f010ddae8bf8bf948c85fe31fc416ead2ede9f
Kamil <kamgre7@gmail.com>
2023-06-12 17:07:29 +0200

    Update controller

--- controllers/TranslateController.ts
+++ controllers/TranslateController.ts
@@ -1,30 +1,29 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 import {
   ITranslateService,
   translateService,
 } from '../services/TranslateService';
-import { PostReqBody } from '../models/postReqBodySchema';
+import { PostTranslateReq } from '../models/postTranslateSchema';
 
-export type ObjectToTranslate = {
-  [key: string]: string;
-};
+export type ObjectToTranslate = Record<string, string>;
 
 export interface ITranslateController {
-  postTranslate(req: Request, res: Response): Promise<void>;
+  postTranslate(req: PostTranslateReq, res: Response): Promise<void>;
 }
 
 export class TranslateController implements ITranslateController {
   constructor(private readonly translateService: ITranslateService) {}
 
-  postTranslate = async (req: Request, res: Response): Promise<void> => {
-    const { textObj, targetLanguage, fromLanguage }: PostReqBody = req.body;
-
-    const from = fromLanguage ?? null;
+  postTranslate = async (
+    req: PostTranslateReq,
+    res: Response
+  ): Promise<void> => {
+    const { textObj, targetLanguage, fromLanguage } = req.body;
 
     const result = await this.translateService.translate(
       textObj,
       targetLanguage,
-      from
+      fromLanguage
     );
 
     res.json(result);
