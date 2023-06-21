914d2daa82a919e40e49e882042d0dcfeb6dcc1f
Kamil <kamgre7@gmail.com>
2023-06-21 17:40:53 +0200

    TranslateService unit tests

--- tests/translateService.test.ts
+++ tests/translateService.test.ts
@@ -11,25 +11,30 @@ describe('Translate service', () => {
   let translatorMock: ITranslator;
   let fileHandlerMock: IFileHandler;
   let translateService: ITranslateService;
-  let objToTranslate: ObjectToTranslate;
-  let translateInstance: Translate;
+  let toTranslate: ObjectToTranslate;
 
   beforeEach(() => {
-    objToTranslate = {
+    jest.mock('@google-cloud/translate/build/src/v2', () => {
+      return {
+        Translate: jest.fn().mockImplementation(() => ({
+          detect: jest.fn().mockReturnValue('en'),
+          translate: jest.fn().mockReturnValue('translated text'),
+        })),
+      };
+    });
+
+    toTranslate = {
       text1: 'Monday',
       text2: 'Tuesday',
     };
 
-    translateInstance = new Translate();
-    translateInstance.translate = jest.fn();
-
     translatorMock = {
-      translate: translateInstance,
+      translate: new (Translate as jest.MockedClass<typeof Translate>)(),
+      detectLanguage: jest.fn().mockResolvedValue('en'),
       translateText: jest
         .fn()
         .mockResolvedValueOnce('Montag')
         .mockResolvedValueOnce('Dienstag'),
-      detectLanguage: jest.fn().mockResolvedValue('en'),
     };
 
     fileHandlerMock = {
@@ -47,16 +52,51 @@ describe('Translate service', () => {
 
   describe('Translate Service', () => {
     describe('Detect language', () => {
-      it('Should call method when languageFrom is null', () => {
-        translateService.translate(objToTranslate, 'de', null);
+      it('Should call detectLanguage method when languageFrom is null', async () => {
+        await translateService.translate(toTranslate, 'de', null);
 
         expect(translatorMock.detectLanguage).toBeCalledTimes(1);
       });
 
-      it('Should not call when language is given', () => {
-        translateService.translate(objToTranslate, 'de', 'en');
+      it('Should not call detectLanguage when language is given', async () => {
+        await translateService.translate(toTranslate, 'de', 'en');
 
         expect(translatorMock.detectLanguage).not.toBeCalled();
+      });
+    });
+
+    describe('Translator API', () => {
+      it('Should call translator API, when cache data not exist', async () => {
+        await translateService.translate(toTranslate, 'de', 'en');
+
+        expect(translatorMock.translateText).toBeCalledTimes(2);
+      });
+
+      it('Should not call translator API, when data from cache match the query ', async () => {
+        fileHandlerMock.readFile = jest.fn().mockResolvedValue({
+          'en-de': [
+            ['Monday', 'Montag'],
+            ['Tuesday', 'Dienstag'],
+          ],
+        });
+
+        await translateService.translate(toTranslate, 'de', 'en');
+
+        expect(translatorMock.translateText).not.toBeCalled();
+      });
+    });
+
+    describe('FileHandler', () => {
+      it('Should call readFile', async () => {
+        await translateService.translate(toTranslate, 'de', 'en');
+
+        expect(fileHandlerMock.readFile).toBeCalledTimes(1);
+      });
+
+      it('Should call writeFile', async () => {
+        await translateService.translate(toTranslate, 'de', 'en');
+
+        expect(fileHandlerMock.writeFile).toBeCalledTimes(1);
       });
     });
   });
